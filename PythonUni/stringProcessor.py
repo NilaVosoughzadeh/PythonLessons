7d72fca8c335850f8084852eccf40592302b8e93
Nila <nilavosoughzadeh@gmail.com>
2026-05-26 18:06:33 +0330

    26.5.2026

---
+++ PythonUni/stringProcessor.py
@@ -0,0 +1,10 @@
+text = input("enter reshte: ")
+sub = input("enter zir reshte: ")
+count = text.count(sub)
+print("Tekrar", count)
+
+#2
+text = input("enter string: ")
+n = int(input("enter number n: "))
+new_text = text[n:]
+print("new string: ", new_text)
